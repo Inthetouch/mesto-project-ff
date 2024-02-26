fb61cf23157fa2027367630ceff7b1f3a458795c
Inthetouch <nikita_eremeev@mail.ru>
2024-02-26 23:42:25 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+let placeList = document.querySelector('.places__list');
+let cardTemplate = document.querySelector('#card-template').content;
+
+
+function addCards() {
+  initialCards.forEach((cards) => {
+    let newCard = cardTemplate.cloneNode(true);
+    let cardImage = newCard.querySelector('.card__image');
+    let cardTitle = newCard.querySelector('.card__title');
+    let cardDelete = newCard.querySelector('.card__delete-button');
+
+    cardImage.src = cards.link;
+    cardImage.alt = cards.name;
+    cardTitle.textContent = cards.name;
+    cardDelete.addEventListener('click', cardRemove);
+
+    placeList.append(newCard);
+  });
+}
+
+function cardRemove(event) {
+  event.target.parentElement.remove();
+}
+
+addCards();
+
