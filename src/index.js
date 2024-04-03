c080879ca8cdef392c1464395903efa2ab8dfc99
Inthetouch <nikita_eremeev@mail.ru>
2024-04-03 23:32:22 +0300

    add feature

--- src/index.js
+++ src/index.js
@@ -33,21 +33,30 @@ function openClosePopup(event) {
   const openPopup = document.querySelector('.popup.popup_type_edit');
   const closePopup = document.querySelector('.popup__close');
   const popupContent = document.querySelector('.popup__content');
+  const darkOverlay = document.querySelector('.popup');
 
-
+//Проверка что была нажата иконка редактирования профиля
+//В случае если нет, то проверяет на нажатие "Х"
   if (event.target === editProfile) {
     openPopup.classList.add('popup_is-opened');
-  } else if (event.target === closePopup) {
+  } else if (event.target === closePopup || event.target === darkOverlay) {
     openPopup.classList.remove('popup_is-opened');
   } 
-
+//Слушатель на нажатие клавишы Escape
   document.addEventListener('keydown', function(event) {
     const key = event.key
     if (key === 'Escape') {
       openPopup.classList.remove('popup_is-opened');
     }
   });
+//Слушатель клика на оверлей
+  darkOverlay.addEventListener('click', function(event) {
+    if (event.target === darkOverlay) {
+      openPopup.classList.remove('popup_is-opened');
+    }
+  })
 
+//Слушатель на нажатие крестика для закрытия popup
   popupContent.addEventListener('click', openClosePopup);
 }
 
