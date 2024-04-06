f2d9b0c31290e49a2cd1a79c425fd72ee94da511
Inthetouch <nikita_eremeev@mail.ru>
2024-04-06 21:55:17 +0300

    new feauture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placeList = document.querySelector('.places__list');
-const editProfile = document.querySelector('.profile__edit-button');
-
-
+const editProfileButton = document.querySelector('.profile__edit-button');
+const editPopup = document.querySelector('.popup.popup_type_edit');
+const formElement = editPopup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_description');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const buttonAddCard = document.querySelector('.profile__add-button');
+const handleAddCard = document.querySelector('.popup_type_new-card');
+const placeNameInput = document.querySelector('.popup__input_type_card-name');
+const linkPlaceNameInput = document.querySelector('.popup__input_type_url');
 
 
 
@@ -27,37 +35,67 @@ function deleteCard(event) {
 initialCards.forEach((item) => {
   const addCard = createCard(item, { deleteCard });
   placeList.append(addCard);
-})
+});
 
-function openClosePopup(event) {
-  const openPopup = document.querySelector('.popup.popup_type_edit');
-  const closePopup = document.querySelector('.popup__close');
-  const popupContent = document.querySelector('.popup__content');
-  const darkOverlay = document.querySelector('.popup');
 
-//Проверка что была нажата иконка редактирования профиля
-//В случае если нет, то проверяет на нажатие "Х"
-  if (event.target === editProfile) {
-    openPopup.classList.add('popup_is-opened');
-  } else if (event.target === closePopup || event.target === darkOverlay) {
-    openPopup.classList.remove('popup_is-opened');
-  } 
-//Слушатель на нажатие клавишы Escape
-  document.addEventListener('keydown', function(event) {
-    const key = event.key
-    if (key === 'Escape') {
-      openPopup.classList.remove('popup_is-opened');
-    }
-  });
-//Слушатель клика на оверлей
-  darkOverlay.addEventListener('click', function(event) {
-    if (event.target === darkOverlay) {
-      openPopup.classList.remove('popup_is-opened');
-    }
-  })
 
-//Слушатель на нажатие крестика для закрытия popup
-  popupContent.addEventListener('click', openClosePopup);
+
+//Функция окрытия окна редактирования
+function openPopup() {
+  formProfileValue();
+  editPopup.classList.add('popup_is-opened');
+
+  //Слушатель на нажатие кнопки "Сохранить"
+  formElement.addEventListener('submit', valueProfileSubmit); 
 }
 
-editProfile.addEventListener('click', openClosePopup);
+//Слушатель на кнопку редактировать профиль
+editProfileButton.addEventListener('click', openPopup);
+
+//Функция закрывающая окна редактирования
+function closePopup(element) {
+  element.classList.remove('popup_is-opened');
+}
+
+//Слушатель на нажатие кнопки Escape при открытом окне
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape' && editPopup.classList.contains('popup_is-opened')) {
+    closePopup(editPopup);
+  } else if (event.key === 'Escape' && handleAddCard.classList.contains('popup_is-opened')) {
+    closePopup(handleAddCard);
+  }
+});
+
+//Слушатель отслеживающий нажатие крестика и клика на overlay
+document.querySelectorAll('.popup').forEach(function(popup) {
+  popup.addEventListener('click', function(event) {
+    if (event.target.classList.contains('popup') || event.target.classList.contains('popup__close')) {
+      closePopup(popup);
+    }
+  });
+});
+
+
+//Функция сохранения значений из форм
+function formProfileValue() {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+}
+
+//Функция сохранения введенных значений 
+function valueProfileSubmit(event) {
+    event.preventDefault(); 
+
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+
+    closePopup(editPopup);
+}
+
+//Функция открытия формы карточки
+function buttonAddNewCard() {
+  handleAddCard.classList.add('popup_is-opened')
+}
+
+//Слушатель на кнопку "+"
+buttonAddCard.addEventListener('click', buttonAddNewCard);
